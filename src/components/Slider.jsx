e9ed7833d5f362185879a07343d83149f019d7f1
mabdullahjs <mabdullah2037@outlook.com>
2024-05-15 21:40:23 +0500

    section 1 is started

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,34 +1,34 @@
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
 
 
-function Mycarousel() {
-  return (
-    <Carousel>
-      <Carousel.Item>
-       <img src="	https://d1iv6qgcmtzm6l.cloudfront.net/banner_desktops/jags2Jp3QiLRBel8jRIPEiDMI2UxTO4nFxaaM8ze.png" alt="" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+// function Mycarousel() {
+//   return (
+//     <Carousel>
+//       <Carousel.Item>
+//        <img src="	https://d1iv6qgcmtzm6l.cloudfront.net/banner_desktops/jags2Jp3QiLRBel8jRIPEiDMI2UxTO4nFxaaM8ze.png" alt="" />
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
   
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
    
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
 
-export default Mycarousel;
+// export default Mycarousel;
